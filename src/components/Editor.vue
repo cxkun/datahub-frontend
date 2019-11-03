29f82ee8963f755fbd1d291f457fdc087f7944eb
Jensen <suiterchik@gmail.com>
2019-11-04 00:01:15 +0800

    init

---
+++ src/components/Editor.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="hello">
+        <div id="editor" style="height: 600px"></div>
+    </div>
+</template>
+
+<script>
+    import * as monaco from 'monaco-editor'
+
+    export default {
+        name: 'HelloWorld',
+        mounted() {
+            monaco.editor.create(document.getElementById('editor'), {
+                value: 'console.log("Hello, world")',
+                language: 'javascript',
+                theme: 'vs-dark'
+            });
+        },
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
