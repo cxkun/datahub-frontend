0f17e2982deb7e82d1ab3cb9252dc6aec57e52e7
Jensen <suiterchik@gmail.com>
2019-11-05 00:02:14 +0800

    初步完成页面layout设计

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,186 @@
+<style>
+
+    /*左边菜单栏 icon 在收起时居中*/
+    .collapsed-menu .ivu-menu-item {
+        padding-left: 10px !important;
+        padding-top: 30px !important;
+    }
+
+    /*去掉左边子菜单在收起时，以及顶部菜单的展开箭头*/
+    .collapsed-menu i.ivu-icon-ios-arrow-down, .ivu-layout-header i.ivu-icon-ios-arrow-down {
+        display: none !important;
+    }
+
+    /*左边子菜单图标居中*/
+    .collapsed-menu .ivu-menu-submenu-title .ivu-icon {
+        margin-left: -15px;
+    }
+
+    /*顶部菜单头像居中*/
+    .ivu-layout-header .ivu-menu-submenu-title span > i {
+        margin-right: 0;
+    }
+
+    /*头部菜单背景色*/
+    .ivu-layout-header, .ivu-menu-horizontal {
+        background: #515a6e;
+    }
+
+    /*左边菜单背景色*/
+    .ivu-layout-sider, .ivu-menu-vertical, .ivu-layout-sider-trigger {
+        background: #5b6270;
+    }
+
+
+    /*左边子菜单打开时样式*/
+    .menu-item span {
+        display: inline-block;
+        overflow: hidden;
+        width: 69px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        vertical-align: bottom;
+        transition: width .2s ease .2s;
+    }
+
+    .menu-item i {
+        transform: translateX(0px);
+        transition: font-size .2s ease, transform .2s ease;
+        vertical-align: middle;
+        font-size: 16px;
+    }
+
+    /*左边子菜单收起时样式*/
+    .collapsed-menu span {
+        width: 0;
+        transition: width .2s ease;
+    }
+
+    .collapsed-menu i {
+        transform: translateX(5px);
+        transition: font-size .2s ease .2s, transform .2s ease .2s;
+        vertical-align: middle;
+        font-size: 22px;
+    }
+
+    .layout {
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+
+
+    .layout-footer-center {
+        text-align: center;
+    }
+</style>
+
 <template>
-    <div id="app">
-        <router-view/>
+    <div class="layout">
+        <Layout>
+            <Header style="height: 50px">
+                <Menu mode="horizontal" theme="dark" active-name="1" style="height: 50px">
+                    <div style="float: right;">
+                        <Submenu name="user">
+                            <template slot="title">
+                                <Avatar icon="ios-person" style="margin-bottom: 10px"/>
+                            </template>
+                            <MenuItem name="my_table">我的资产</MenuItem>
+                            <MenuItem name="my_album">我的专辑</MenuItem>
+                            <MenuItem name="user_guide">用户手册</MenuItem>
+                            <MenuItem name="logout">退出</MenuItem>
+                        </Submenu>
+                    </div>
+                </Menu>
+            </Header>
+            <Layout :style="{minHeight: '100vh'}">
+                <Sider collapsible :collapsed-width="50" v-model="isCollapsed" :width="180">
+                    <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
+                        <MenuItem name="DataDevelopment">
+                            <Icon type="md-construct"></Icon>
+                            <span>数据开发</span>
+                        </MenuItem>
+                        <MenuItem name="TempQuery">
+                            <Icon type="md-search"></Icon>
+                            <span>临时查询</span>
+                        </MenuItem>
+
+                        <Submenu name="ReleaseCenter">
+                            <template slot="title">
+                                <Icon type="md-paper-plane"></Icon>
+                                <span>发布中心</span>
+                            </template>
+                            <MenuItem name="CreatePackage">创建发布包</MenuItem>
+                            <MenuItem name="ListPackage">发布包列表</MenuItem>
+                        </Submenu>
+
+                        <MenuItem name="DataQuality">
+                            <Icon type="md-bug"/>
+                            <span>质量监控</span>
+                        </MenuItem>
+
+                        <Submenu name="OpsCenter">
+                            <template slot="title">
+                                <Icon type="md-pulse"></Icon>
+                                <span>运维中心</span>
+                            </template>
+                            <MenuItem name="OpsOverview">运维大屏</MenuItem>
+                            <MenuItem name="OpsRuntime">实时运维</MenuItem>
+                            <MenuItem name="OpsRoutine">周期运维</MenuItem>
+                        </Submenu>
+
+
+                        <Submenu name="DataMap">
+                            <template slot="title">
+                                <Icon type="md-map"></Icon>
+                                <span>数据地图</span>
+                            </template>
+                            <MenuItem name="TableList">数据资产</MenuItem>
+                            <MenuItem name="TableDependence">血缘分析</MenuItem>
+                            <MenuItem name="TableAlbum">数据专辑</MenuItem>
+                        </Submenu>
+
+                        <MenuItem name="FunctionCenter">
+                            <Icon type="logo-foursquare"/>
+                            <span>函数中心</span>
+                        </MenuItem>
+
+                        <MenuItem name="Trash">
+                            <Icon type="md-trash"></Icon>
+                            <span>回收站</span>
+                        </MenuItem>
+                    </Menu>
+                </Sider>
+                <router-view/>
+
+
+            </Layout>
+
+
+        </Layout>
+
+
     </div>
 </template>
 
 <script>
     export default {
-        name: 'App'
+        data() {
+            return {
+                isCollapsed: true,
+            };
+        },
+        computed: {
+            menuitemClasses: function () {
+                return [
+                    'menu-item',
+                    this.isCollapsed ? 'collapsed-menu' : ''
+                ]
+            }
+        },
+        methods: {}
     }
 </script>
 
-<style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-</style>
