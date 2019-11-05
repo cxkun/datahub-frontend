4857aa8726a2ecebb23f5beca7229e24b5efb769
Jensen <suiterchik@gmail.com>
2019-11-06 00:20:05 +0800

    初始化工具栏样式

---
+++ src/components/data-development/ToolBar.vue
@@ -0,0 +1,18 @@
+<style scoped>
+    .buttons {
+        font-size: large;
+    }
+
+</style>
+<template>
+    <ButtonGroup>
+        <Button class="buttons" icon="md-play"></Button>
+        <Button class="buttons" icon="md-remove-circle"></Button>
+        <Button class="buttons" icon="md-lock"></Button>
+        <Button class="buttons" icon="md-cloud-upload"></Button>
+        <Button class="buttons" icon="md-paper-plane"></Button>
+        <Button class="buttons" icon="md-alarm"></Button>
+        <Button class="buttons" icon="md-git-branch"></Button>
+        <Button class="buttons" icon="ios-albums"></Button>
+    </ButtonGroup>
+</template>
