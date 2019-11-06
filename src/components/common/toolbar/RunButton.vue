6ad4ae2db62a205c0c1a951387e2a55552592189
Jensen <suiterchik@gmail.com>
2019-11-07 01:11:46 +0800

    添加执行按钮

---
+++ src/components/common/toolbar/RunButton.vue
@@ -0,0 +1,34 @@
+<style scoped>
+
+</style>
+<template>
+    <Tooltip content="执行">
+        <Button class="toolbar-button" icon="md-play" @click="modalIsOpen=true"/>
+        <Modal
+            v-model="modalIsOpen"
+            title="执行参数"
+            @on-ok="startJob"
+            @on-cancel="cancel">
+            <p>Content of dialog</p>
+            <p>Content of dialog</p>
+            <p>Content of dialog</p>
+        </Modal>
+    </Tooltip>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                modalIsOpen: false
+            }
+        },
+        methods: {
+            startJob() {
+
+            },
+            cancel() {
+                this.modalIsOpen = false
+            }
+        }
+    }
+</script>
