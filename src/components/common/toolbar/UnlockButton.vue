049cd6d459489103f32cd8b304131019afaf29b0
Jensen <suiterchik@gmail.com>
2019-11-07 01:13:01 +0800

    添加解锁按钮

---
+++ src/components/common/toolbar/UnlockButton.vue
@@ -0,0 +1,21 @@
+<style scoped>
+
+</style>
+<template>
+    <Tooltip content="解锁编辑">
+        <Button class="toolbar-button" icon="md-unlock" @click="unlock"/>
+    </Tooltip>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+            }
+        },
+        methods: {
+            unlock(){
+
+            }
+        }
+    }
+</script>
