2d9ba028d0a315a28c15f698bde519c63ef69368
Jensen <suiterchik@gmail.com>
2019-11-11 23:19:51 +0800

    初始化调度配置按钮

---
+++ src/components/common/toolbar/SchedulerButton.vue
@@ -0,0 +1,27 @@
+<style scoped>
+
+</style>
+<template>
+    <Tooltip content="调度配置" placement="top" theme="light">
+        <Button class="toolbar-button" icon="md-alarm" @click="openDrawer"/>
+        <Drawer title="调度配置" :closable="false" v-model="drawerIsOpen" width="40">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+        </Drawer>
+    </Tooltip>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                drawerIsOpen: false,
+            }
+        },
+        methods: {
+            openDrawer() {
+                this.drawerIsOpen = true;
+            }
+        }
+    }
+</script>
