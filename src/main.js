a22c0a06e43a95f2577383ecde43f5070e44f889
Jensen Qi <SuiterChik@gmail.com>
2020-01-16 00:33:01 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App'
 import router from './router'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
 import Plotly from 'plotly.js';
 
+axios.defaults.baseURL = '/api/';
+Vue.use(VueAxios, axios);
 Vue.use(ViewUI);
 Vue.prototype.$Plotly = Plotly;
 
