2493b613dbd35d6831531076b74cf1b8d989e665
Jensen Qi <SuiterChik@gmail.com>
2020-02-27 02:00:46 +0800

    添加请求响应拦截处理

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
@@ -10,19 +8,29 @@ import 'view-design/dist/styles/iview.css';
 import Plotly from 'plotly.js';
 
 axios.defaults.baseURL = '/api/';
+
 axios.interceptors.request.use(config => {
-    if (localStorage.token) {
-        config.headers.token = localStorage.token
-    }
+    config.headers.token = localStorage.token;
     return config
+
 });
+axios.interceptors.response.use(response => {
+    if (response.data.status !== 'success') {
+        console.log("error")
+    } else {
+        return response.data.data;
+    }
+}, error => {
+    console.log(error)
+    return Promise.reject(error);
+});
+
 Vue.use(VueAxios, axios);
 Vue.use(ViewUI);
 Vue.prototype.$Plotly = Plotly;
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
