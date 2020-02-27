2d2ea70544be03688ea6ffb658410a5c8b3d23a1
Jensen Qi <SuiterChik@gmail.com>
2020-02-28 00:53:03 +0800

    初始化项目组选择下拉框组件

---
+++ src/components/common/selections/GroupSelection.vue
@@ -0,0 +1,35 @@
+<style scoped>
+
+</style>
+
+<template>
+    <Select style="width:120px;margin-top: -10px;margin-left: 20px" v-model="selectedGroup">
+        <Option v-for="group in groups" :value="group.id">
+            {{ group.name }}
+        </Option>
+    </Select>
+</template>
+
+<script>
+    export default {
+        name: "GroupSelection",
+        beforeMount() {
+            this.axios.get("/group").then(data => {
+                this.groupsCount = data.count;
+                this.groups = data.groups;
+                if (this.groups.length > 0) {
+                    this.selectedGroup = this.groups[0].id
+                }
+            })
+        },
+        data() {
+            return {
+                groupsCount: 0,
+                groups: [],
+                selectedGroup: null
+            }
+        }
+    }
+</script>
+
+
