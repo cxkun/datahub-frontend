e3a192396b05c0af70280ade8f30096b6a539d02
Jensen Qi <SuiterChik@gmail.com>
2020-03-07 15:02:40 +0800

    实现项目组组件的增删改查

--- src/components/manager-center/GroupManager.vue
+++ src/components/manager-center/GroupManager.vue
@@ -4,11 +4,46 @@
 
 <template>
     <Card style="margin: 30px;">
+        <Row>
+            <Button type="primary" @click="createGroupModal.visible=true">
+                新建
+                <Modal v-model="createGroupModal.visible" @on-ok="createGroup"
+                       @on-cancel="createGroupModal.visible = false">
+                    <div>
+                        <Icon type="md-alert" style="font-size: x-large; color: #2db7f5"/>
+                        <span style="font-size: large; margin-left: 5px"> 新建项目组 </span>
+                    </div>
+                    <Form :label-width="100" style="margin-top: 30px">
+                        <FormItem label="项目组名称">
+                            <Input v-model="createGroupModal.name" :maxlength="256" show-word-limit/>
+                        </FormItem>
+                    </Form>
+                </Modal>
+            </Button>
 
-        <Table :columns="columns" :data="groups" style="margin-top: 30px;"/>
+
+            <Modal v-model="updateGroupModal.visible" @on-ok="updateGroup"
+                   @on-cancel="updateGroupModal.visible = false">
+                <div>
+                    <Icon type="md-alert" style="font-size: x-large; color: #2db7f5"/>
+                    <span style="font-size: large; margin-left: 5px"> 更新项目组信息 </span>
+                </div>
+                <Form :label-width="60" style="margin-top: 30px">
+                    <FormItem label="项目组名称">
+                        <Input v-model="updateGroupModal.name" :maxlength="256" show-word-limit/>
+                    </FormItem>
+                </Form>
+            </Modal>
+
+            <Col span="6" style="float: right">
+                <Input v-model="search.key" search @on-enter="() => this.changePage(1)"/>
+            </Col>
+        </Row>
+
+        <Table :columns="columns" :data="groups" :loading="loading" style="margin-top: 30px;"/>
 
         <div style="text-align: right; margin-top: 20px">
-            <Page :total="this.groupCount" show-sizer show-total
+            <Page :total="this.groupCount" :current="pageId" show-sizer show-total
                   @on-change="changePage"
                   @on-page-size-change="changePageSize"/>
         </div>
@@ -17,6 +52,11 @@
 </template>
 
 <script>
+    /**
+     * 项目组管理
+     * 普通用户：查看项目组 & 申请加入项目组
+     * 超级管理员：增删改查项目组
+     */
     export default {
         name: "GroupManager",
         beforeMount() {
@@ -24,46 +64,151 @@
         },
         data() {
             return {
+                loading: false,
                 columns: [
                     {
                         title: '项目组 ID',
                         key: 'id',
-                        width: 150
+                        width: 150,
+                        align: 'center'
                     },
                     {
                         title: '项目组名称',
                         key: 'name',
+                        align: 'center'
                     },
                     {
                         title: '创建时间',
-                        key: 'createTime'
+                        key: 'createTime',
+                        align: 'center'
                     },
                     {
                         title: '更新时间',
-                        key: 'updateTime'
+                        key: 'updateTime',
+                        align: 'center'
                     },
                     {
-                        title: '操作'
+                        title: '操作',
+                        align: 'center',
+                        width: 150,
+                        render: (h, {row, col, index}) => {
+                            return h('div', [
+                                h('Button', {
+                                    props: {
+                                        type: 'info',
+                                        size: 'small',
+                                        shape: 'circle',
+                                        icon: 'md-create'
+                                    },
+                                    on: {
+                                        'click': () => {
+                                            this.updateGroupModal.id = row.id;
+                                            this.updateGroupModal.name = row.name;
+                                            this.updateGroupModal.rowIndex = index;
+                                            this.updateGroupModal.visible = true;
+                                        }
+                                    }
+                                }),
+                                h('Poptip', {
+                                        props: {
+                                            title: `确认删除项目组 ${row.name} ?`,
+                                            confirm: true,
+                                            transfer: true
+                                        },
+                                        on: {
+                                            'on-ok': () => {
+                                                this.axios.delete(`/group/${row.id}`).then(data => {
+                                                    this.groups.splice(index, 1);
+                                                    //todo: 向下补全
+                                                });
+                                            }
+                                        }
+                                    }, [h('Button', {
+                                        props: {
+                                            type: 'error',
+                                            size: 'small',
+                                            shape: 'circle',
+                                            icon: 'md-trash'
+                                        },
+                                        style: {
+                                            marginLeft: '5px'
+                                        }
+                                    })]
+                                ),
+                            ])
+
+                        }
                     }
                 ],
+                search: {
+                    key: null
+                },
                 groups: [],
                 groupCount: 0,
                 pageSize: 10,
-                pageId: 1
+                pageId: 1,
+                createGroupModal: {
+                    name: null,
+                    visible: false
+                },
+                updateGroupModal: {
+                    id: null,
+                    name: null,
+                    rowIndex: null,
+                    visible: false
+                }
             }
         },
         methods: {
-            changePage(pageId) {
-                this.axios.get("/group", {
-                    params: {
-                        page: pageId,
-                        pageSize: this.pageSize
+            createGroup() {
+                let params = new FormData();
+                params.set("name", this.createGroupModal.name);
+                this.axios.post("/group", params).then(data => {
+                    this.groupCount += 1;
+                    if (this.groups.length < this.pageSize) {
+                        this.groups.push(data.group)
                     }
-                }).then(data => {
+                    this.createGroupModal = {
+                        name: null,
+                        visible: false
+                    }
+                });
+            },
+
+            updateGroup() {
+                let params = new FormData();
+                params.set("id", this.updateGroupModal.id);
+                params.set("name", this.updateGroupModal.name);
+                this.axios.put(`/group/${this.updateGroupModal.id}`, params).then(data => {
+                    this.$set(this.groups, this.updateGroupModal.rowIndex, data.group);
+                    this.updateGroupModal = {
+                        id: null,
+                        name: null,
+                        rowIndex: null,
+                        visible: false
+                    }
+                });
+            },
+
+            changePage(pageId) {
+                this.loading = true;
+                let params = {
+                    page: pageId,
+                    pageSize: this.pageSize
+                };
+                if (this.search.key !== null && this.search.key.trim() !== '') {
+                    params.like = this.search.key;
+                }
+                this.axios.get("/group", {params: params}).then(data => {
                     this.groupCount = data.count;
                     this.groups = data.groups;
+                    this.pageId = pageId;
+                    this.loading = false;
+                }).catch(err => {
+                    this.loading = false;
                 })
             },
+
             changePageSize(pageSize) {
                 this.pageSize = pageSize;
                 this.changePage(1);
